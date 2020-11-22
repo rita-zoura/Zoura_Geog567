aa21bdd9a8efe59c8aa9e1ec8ff65f7ece57a0d5
rita-zoura <61521464+rita-zoura@users.noreply.github.com>
2020-11-22 13:58:46 -0700

    stuffs

--- Zoura_Lab9_Script1.py
+++ Zoura_Lab9_Script1.py
@@ -1,35 +1,60 @@
-# Lab 7
+# Lab 9
 # Rita Zoura | UCID: 30018933 | rita.zoura@ucalgary.ca
 # Run using Python 3.6
 # Vector script
 
+# Import arcpy
 import arcpy
 
+# function to get the workspace from the user
+# no parameters
 def GetWorkspace():
     workspace = input("What is the location of the workspace folder?: ")
+    # returns the workspace location
     return workspace
 
+# function to get the list of features and set the workspace location
 def GetFeatures(file_location):
+    # Sets the workspace
     arcpy.env.workspace = workspace
     
+    # list of feature calsses in the workspace
     featureClasses = arcpy.ListFeatureClasses()
+
+    # returns the list of feature calsses found in the workspace
     return featureClasses
 
-
+# function defines projections for shapefiles missing prjection information.
+# outputs the information to the user in the terminal
+# if a shapefiel contains a spatial reference then it prints it to the terminal
+# if the shapfile doens not it difeines the projection to NAD 193 UTM Zone 12N 
+# and print it to the terminal and user.
 def DefineProjections(shapefile_list):
+    # initialize list of fiels with known spatial reference
     known_sr_list = []
+    # initializes list of files with unknown spatial reference
     unknown_sr_list = []
+
+    # sets a new spatial reference to NAD 1983 UTM Zone 12N
     new_sr = arcpy.SpatialReference("NAD 1983 UTM Zone 12N")
 
+    # goes through the list and
     for item in shapefile_list:
+        # describes the spatial reference of the file
         sr = arcpy.Describe(item).spatialReference
+        # if it isn't "unknown"
         if sr.name != "Unknown":
+            # then it gets added the list of fiels with knwon spatial references
             known_sr_list.append(item)
+        
+        # otherwise
         else:
+            # the fiel gets added to the list of files with unknown spatial references
             unknown_sr_list.append(item)
+            # and the spatialreference is defined to NAD 1983 UTM Zone 12N
             sr = arcpy.DefineProjection_management(item, new_sr)
 
-
+    # Output the information about to the user in an orginized matter
     print("Known Projections: \n")
     for item in known_sr_list:
         sr = arcpy.Describe(item).spatialReference
@@ -40,14 +65,19 @@ def DefineProjections(shapefile_list):
         sr = arcpy.Describe(item).spatialReference
         print("\t" + item + ": Spatial Reference unknown, defined projection to: " + sr.name)
 
-
+# activates getWorkspace function
 workspace = GetWorkspace()
 
+# activates get features function
 feature_classes = GetFeatures(workspace)
 
+# gets the number of features in the workspace
 feature_classes_count = len(feature_classes)
 
+# Activates teh define projection function
 DefineProjections(feature_classes)
+
+# prints the following to the user.
 print("There are " + str(feature_classes_count) + " features in this workspace.")
 print("Script Complete!")
 
--- Zoura_Lab9_Script2.py
+++ Zoura_Lab9_Script2.py
@@ -3,29 +3,46 @@
 # Run using Python 3.6
 # Raster script
 
+# Imports Arcpy
 import arcpy
 
+# Gets the workspace location from the user
 workspace = input("What is the location of your workspace folder?: ")
+# sets the workspace
 arcpy.env.workspace = workspace
 
+# gets a list of rasters in workspace
 rasters = arcpy.ListRasters()
-
+# initializes number of bands in workspace
 band_count = 0
-
+# go throug heach raster in list
 for raster in rasters:
+    # describe the raster
     information = arcpy.Describe(raster)
+    # if the format is text based
     if information.format == "AFR":
+        # remove it from the list
         rasters.remove(raster)
+    # otherwise
     else:
+        # get the name
         name = information.name
+        # get teh file type
         file_type = information.format
+        # get teh number of rows
         height = information.height
+        # get the number of columns
         width = information.width
+        # does it contain a band
         has_band = information.bandCount
+        # if it does at plus 1 to the band count
         if has_band > 0:
             band_count += 1
+    
+    # out the information neatly to user in the terminal
     print("File " + name + ":")
     print("\t file type: " + file_type)
     print("\t dimensions: " + str(width) + " x " + str(height))
 
+# output the number of bunds int he workspace to the user
 print("There are " + str(band_count) + " bands in this folder")
