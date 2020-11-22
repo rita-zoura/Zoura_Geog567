e7896042fea041dc461be82dd63720a5fa6887c7
rita-zoura <61521464+rita-zoura@users.noreply.github.com>
2020-11-22 13:28:43 -0700

    completes script 1 and 2 comments and code

--- Zoura_Lab10_Script2.py
+++ Zoura_Lab10_Script2.py
@@ -20,21 +20,23 @@ arcpy.env.workspace = arcpy.GetParameterAsText(0)
 rasters = arcpy.ListRasters()
 
 band_count = 0
+if len(rasters) > 0:
+    for raster in rasters:
+        information = arcpy.Describe(raster)
+        if information.format == "AFR":
+            rasters.remove(raster)
+        else:
+            name = information.name
+            file_type = information.format
+            height = information.height
+            width = information.width
+            has_band = information.bandCount
+            if has_band > 0:
+                band_count += 1
+        arcpy.AddMessage("File " + name + ":")
+        arcpy.AddMessage("\t file type: " + file_type)
+        arcpy.AddMessage("\t dimensions: " + str(width) + " x " + str(height))
 
-for raster in rasters:
-    information = arcpy.Describe(raster)
-    if information.format == "AFR":
-        rasters.remove(raster)
-    else:
-        name = information.name
-        file_type = information.format
-        height = information.height
-        width = information.width
-        has_band = information.bandCount
-        if has_band > 0:
-            band_count += 1
-    arcpy.AddMessage("File " + name + ":")
-    arcpy.AddMessage("\t file type: " + file_type)
-    arcpy.AddMessage("\t dimensions: " + str(width) + " x " + str(height))
-
-arcpy.AddMessage("There are " + str(band_count) + " bands in this folder")
+    arcpy.AddMessage("There are " + str(band_count) + " bands in this folder")
+else:
+    arcpy.AddError("There are no rasters in this workspace")
